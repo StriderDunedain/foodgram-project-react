f347b1891a6913915753a220df61ed72ab3c208e
Максим Трухинов <maks.truxinov@yandex.ru>
2023-05-09 18:56:08 +0300

    review-changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='wdj!v*=#*amk4d2!k&nizs6phtwp0eqf3ysmq7j&)xxpwpf2w*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
